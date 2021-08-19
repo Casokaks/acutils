8df3a1fe9b8bc182577710e495d72c833c4b8b31
Casokaks <casokaks@gmail.com>
2021-08-19 21:31:51 +0000

    Update init file.

--- acutils/__init__.py
+++ acutils/__init__.py
@@ -12,3 +12,12 @@ Created on: Aug 15th 2021
 # Library version
 # -----------------------------------------------------------------------------------
 __version__ = "0.2.1"
+
+# imports
+from .df_bulk_rolling import *
+from .generic_function_execution import *
+from .list_utils import *
+from .normalize import *
+from .object_utils import *
+from .plotlib import *
+from .progresslib import *
